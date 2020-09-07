6b3224d6c9151c41de75c7236e010ba3c2f2b469
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2020-09-06 20:44:06 -0400

    exploring skyrim's main esm file contents

--- Project/esparse/main.cpp
+++ Project/esparse/main.cpp
@@ -4,12 +4,40 @@
 #include <iostream>
 #include "Plugin.h"
 
+#define SKYRIM_PLUGIN "Skyrim.esm"
+
 using namespace libespm;
+
+class Skyrim : public Plugin
+{
+    public:
+        inline Skyrim(GameId gameId) : Plugin(gameId)
+        {
+            std::cout << "Hello, world!" << std::endl;
+        }
+        inline const Record& getHeaderRecord()
+        {
+            return headerRecord;
+        }
+};
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
-    Plugin plugin(GameId::SKYRIM);
-    plugin.load("Skyrim.esm");
+    int index = 0;
+    Skyrim plugin(GameId::SKYRIM);
+    plugin.load(SKYRIM_PLUGIN);
+    std::cout << plugin.getFormIds().size() << std::endl;
+    for (const auto& subrecord : plugin.getHeaderRecord().getSubrecords())
+    {
+        std::cout << subrecord.getType() << std::endl;
+    }
+    for (std::set<FormId>::const_iterator iter = plugin.getFormIds().begin(); iter != plugin.getFormIds().end(); iter++)
+    {
+        if (iter->getPluginName() != SKYRIM_PLUGIN)
+        {
+            std::cout << iter->getPluginName() << "@" << index << std::endl;
+        }
+        ++index;
+    }
     return 0;
 }
