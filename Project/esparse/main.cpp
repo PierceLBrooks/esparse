346f691db4567de36bb10f73df01680a7a7b9530
Pierce Lewis Brooks <piercebrooks@rocketmail.com>
2020-09-06 20:26:27 -0400

    dependent objects link to boost ok

--- Project/esparse/main.cpp
+++ Project/esparse/main.cpp
@@ -1,7 +1,15 @@
 
 // Author: Pierce Brooks
 
+#include <iostream>
+#include "Plugin.h"
+
+using namespace libespm;
+
 int main()
 {
+    std::cout << "Hello, world!" << std::endl;
+    Plugin plugin(GameId::SKYRIM);
+    plugin.load("Skyrim.esm");
     return 0;
 }
